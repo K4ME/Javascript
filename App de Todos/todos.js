1fdd83b539a8745091d35674850468cd3f8ad7a0
K4ME <guilhermegodencio@gmail.com>
2020-01-05 10:44:07 -0300

    Iniciando Aplicação

--- App de Todos/todos.js
+++ App de Todos/todos.js
@@ -1,1 +1,9 @@
-alert("funcionou");
+var listElement = document.querySelector("#app ul");
+var inputElement = document.querySelector("#app input");
+var buttonElement = document.querySelector("#app button");
+
+var todos = [
+  "Fazer café",
+  "Estudar Javascript",
+  "Acessar comunidade da Rockeseat"
+];
