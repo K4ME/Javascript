b73cd695e6e13e24b7e79a5e0c57d098391c6130
K4ME <guilhermegodencio@gmail.com>
2020-02-24 07:38:39 -0300

    Template Literals

--- ES6/main.js
+++ ES6/main.js
@@ -1,45 +1,5 @@
-//REST
+const nome = "Guilherme";
+const idade = 23;
 
-const usuario = {
-  nome: "Guilherme",
-  idade: 24,
-  empresa: "SMARAPD"
-};
-
-const { nome, ...resto } = usuario;
-
-console.log(nome);
-console.log(resto);
-//------------------------
-const arr = [1, 2, 3, 4];
-
-const [a, b, ...c] = arr;
-
-console.log(a);
-console.log(b);
-console.log(c);
-//------------------------
-
-function soma(...params) {
-  return params.reduce((total, next) => total + next);
-}
-console.log(soma(1, 3, 4));
-//************************************************************
-
-//SPREAD
-
-const arr1 = [1, 2, 3];
-const arr2 = [4, 5, 6];
-
-const arr3 = [...arr1, ...arr2];
-
-console.log(arr3);
-//------------------------
-const usuario1 = {
-  nome: "Guilherme",
-  idade: 24,
-  empresa: "SMARAPD"
-};
-
-const usuario2 = { ...usuario1, nome: "Gustavo" };
-console.log(usuario2);
+//console.log("meu nome é " + nome + "e tenho " + idade + "anos");
+console.log(`Meu nome é ${nome} e tenho ${idade} anos.`);
