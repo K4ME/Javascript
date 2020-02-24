45879518b3743e6ee174aa56587387e43dfd4488
K4ME <guilhermegodencio@gmail.com>
2020-02-24 06:58:02 -0300

    Arrow Functions

--- ES6/main.js
+++ ES6/main.js
@@ -1,25 +1,12 @@
-const arr = [1, 3, 4, 5, 8, 10];
+const arr = [1, 3, 4, 5, 6];
 
-const newArr = arr.map(function(item, index) {
-  return item + index;
+const newArr = arr.map(function(item) {
+  return item * 2;
 });
 
-console.log(newArr);
+//mesmo funcionamento
+const newArr2 = arr.map(item => item * 2);
+console.log(newArr2);
 
-const sum = arr.reduce(function(total, next) {
-  return total + next;
-});
-
-console.log(sum);
-
-const filter = arr.filter(function(item) {
-  return item % 2 === 0;
-});
-
-console.log(filter);
-
-const find = arr.find(function(item) {
-  return item === 2;
-});
-
-console.log(find);
+const teste = () => ({ nome: "Guilherme" });
+console.log(teste);
