600258bf0aa48b8ce7f10ea5c9e23fcfa1ef626d
K4ME <guilhermegodencio@gmail.com>
2020-02-24 13:04:30 -0300

    Async/await

--- ES6/bundle.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-
-var nome = "Guilherme";
-var idade = 23; //console.log("meu nome é " + nome + "e tenho " + idade + "anos");
-
-console.log("Meu nome \xE9 ".concat(nome, " e tenho ").concat(idade, " anos."));
--- ES6/main.js
+++
@@ -1,5 +0,0 @@
-const nome = "Guilherme";
-const idade = 23;
-
-//console.log("meu nome é " + nome + "e tenho " + idade + "anos");
-console.log(`Meu nome é ${nome} e tenho ${idade} anos.`);
